122cbb8fd48b869d37e67991a75f3fe926e945a2
MilhamAt <M477ashiddiq@gmail.com>
2020-10-23 21:22:18 +0700

    Initial Commit

---
+++ SignInWithApple_Macro/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SignInWithApple_Macro
+//
+//  Created by Muhammad Ilham Ashiddiq Tresnawan on 23/10/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
